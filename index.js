746b582896a31b1f9755d26b4bd9b9c97e0815cb
nest <salvatierra.felix.rogelio@gmail.com>
2025-10-08 11:26:30 -0300

    Traer productos GET

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hola");
+let [,,comando,recurso] = process.argv;
+
+
+comando = comando.toUpperCase();
+recurso = recurso.toLowerCase();
+
+if (comando == "GET" && recurso == "products") {
+    const respuesta = await fetch('https://fakestoreapi.com/products');
+    const datos = await respuesta.json();
+    console.log(datos);
+}
