1831b5d465c5b578f2ca17461717e765fc84fa00
nest <salvatierra.felix.rogelio@gmail.com>
2025-10-08 11:30:08 -0300

    traer productos GET

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// ----- tomar argumentos ----------------
 let [,,comando,recurso] = process.argv;
-
+// ------ validar may,minsc --------------
 
 comando = comando.toUpperCase();
 recurso = recurso.toLowerCase();
-
+// ------ traer productos de Fakestoreapi ------------------
 if (comando == "GET" && recurso == "products") {
     const respuesta = await fetch('https://fakestoreapi.com/products');
     const datos = await respuesta.json();
