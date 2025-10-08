02bc5d9d5350abd4883dc9aa439b7a3536e79349
nest <salvatierra.felix.rogelio@gmail.com>
2025-10-08 12:59:08 -0300

    producto por ID

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ----- tomar argumentos ----------------
+// ^^----- tomar argumentos ----------------
 let [,,comando,recurso] = process.argv;
 // ------ validar may,minsc --------------
 
@@ -9,4 +9,15 @@ if (comando == "GET" && recurso == "products") {
     const respuesta = await fetch('https://fakestoreapi.com/products');
     const datos = await respuesta.json();
     console.log(datos);
+}
+//-------- mostrar un producto por id ------------------------------------
+//-------- toma de argumento y validacion a entro y > 0----
+const match = process.argv.find((arg) => /^products\/\d+$/.test(arg));
+const id = match ? match.split("/")[1] : "--Identificador no valido";
+if (id == 0) {
+    console.log("--Identificador no valido");
+} else {
+    fetch('https://fakestoreapi.com/products/' + id)
+        .then(response => response.json())
+        .then(data => console.log(data));
 }
