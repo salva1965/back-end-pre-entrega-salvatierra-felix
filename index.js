0da8e38b54f0eff421f75676e1cbc8547bb950c6
nest <salvatierra.felix.rogelio@gmail.com>
2025-10-10 12:24:50 -0300

    DELETE {id}

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
-// ------- tomar argumentos ----------------
-let [,,comando,recurso,...parmetros] = process.argv;
-// ------ validar may,minsc --------------
+// // ------- tomar argumentos ----------------
+ let [,,comando,recurso,...parametros] = process.argv;
+ // ------ validar may,minsc --------------
 
 comando = comando.toUpperCase();
 recurso = recurso.toLowerCase();
-//-------- crear un nuevo producto -----------------------------------
-if (comando == "POST" && recurso == "products") {
-    const [title,price,category]= parmetros;
-    const product = { title,price,category};
 
-    fetch('https://fakestoreapi.com/products', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(product),
-    })
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
-// ------ traer productos de Fakestoreapi ------------------
+
+ // ------ traer productos de Fakestoreapi ------------------
 if (comando == "GET" && recurso == "products") {
     const respuesta = await fetch('https://fakestoreapi.com/products');
     const datos = await respuesta.json();
     console.log(datos);
 }
 //-------- mostrar un producto por id ------------------------------------
-//-------- toma de argumento y validacion a entro y > 0----
-const match = process.argv.find((arg) => /^products\/\d+$/.test(arg));
-const id = match ? match.split("/")[1] : "--Identificador no valido";
-if (id == 0) {
-    console.log("--Identificador no valido");
-} else {
-    fetch('https://fakestoreapi.com/products/' + id)
+
+if (comando == "GET" && recurso.startsWith("products/")) {
+    let id = recurso.split("/")[1];
+    id = parseInt(id);
+    if (isNaN(id) || id < 1) {
+        console.log("--Identificador no valido");
+    } else {
+        
+        fetch('https://fakestoreapi.com/products/' + id)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+   
+}
+//-------- Agregar un nuevo producto -----------------------------------
+
+if (comando == "POST" && recurso == "products") {
+    
+    const [title, price, category] = parametros;
+    const product = { title, price, category };
+
+    fetch('https://fakestoreapi.com/products', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(product)
+    })
         .then(response => response.json())
         .then(data => console.log(data));
+}
+//----------- Eliminar un producto ---------------------------------------
+
+if (comando == "DELETE" && recurso.startsWith("products/")) {
+    let id = parseInt(recurso.split("/")[1]);
+    
+    if (isNaN(id) || id < 1) {
+        console.log("--Identificador no valido");
+    } else {
+
+        fetch('https://fakestoreapi.com/products/' + id , {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
 }
